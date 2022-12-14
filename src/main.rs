c27bef9c7ea9d1302191ac149b2ae3c95c1df405
sty <99866356+sty00A4@users.noreply.github.com>
2022-12-14 11:16:55 +0100

    Delete src directory

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-#![allow(unused)]
-mod brainfuck;
-use std::str::Chars;
-
-fn main() {
-    let instrs = brainfuck::Instr::generate(",[>++<-]>.");
-    let mut program = brainfuck::Program::new();
-    program.instrs(&instrs);
-}
